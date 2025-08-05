0e38f1925b65df1dcf2476eff85b3299037b9cad
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2025-08-05 09:36:21 -0700

    Update main.rs

--- topics/variable/exercises/src/main.rs
+++ topics/variable/exercises/src/main.rs
@@ -2,9 +2,13 @@
 // Exercise: Fix the code to make it compile and pass the assertions
 
 fn main() {
+    let test =4;
+    let  mut age = 17;
+    let  name = "jake";
     // Exercise 1: Make this variable mutable
-    let count = 1;
-    count += 1;
-
+    let mut count = 1;
     println!("count: {count}");
+    count += 1;
+    println!("count: {count}");
+    println!("my name is {0} and im {1} now, in {2} years ill be {3}",name,age,test,(age+test));    
 }
