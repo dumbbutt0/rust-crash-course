0a9239e29da12a1f6822651d0c9d3bd6b182199f
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2025-08-05 09:33:23 -0700

    finish math .rs

--- topics/function/exercises/src/lib.rs
+++ topics/function/exercises/src/lib.rs
@@ -1,7 +1,17 @@
-pub fn mul() {
-    todo!();
+#![allow(unused)]
+pub fn mul(x: u32, y: u32) -> u32 {
+    return x * y;
 }
 
-pub fn div() {
-    todo!();
+pub fn div(x: u32, y: u32) -> u32{
+    return y/x;
+}
+
+fn main() {
+    let x = 2;
+    let y = 4;
+    //mul(x,y);
+    //div(x,y);
+    println!("{} * {} = {}",x, y, mul(x,y));
+    println!("{} / {} = {}",y, x, div(x,y));
 }
