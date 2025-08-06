64f946822c989e54e2b25d0f53a93d771e31d348
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2025-08-05 21:53:53 -0700

    finish struct lesson

--- topics/struct_type/exercises/src/lib.rs
+++ topics/struct_type/exercises/src/lib.rs
@@ -1,6 +1,10 @@
 #[derive(Debug)]
-pub struct Account {}
+pub struct Account {
+   pub address: String,
+    pub balance: u32,
+}
 
 pub fn new(address: String) -> Account {
-    todo!();
+    let p = Account{address,balance:0};
+    p
 }
