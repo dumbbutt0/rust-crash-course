7208e61f499a8cb96b6edbbe0b22302a39bec71c
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2025-08-06 01:13:05 -0700

    finish match lesson

--- topics/pattern_match/exercises/src/lib.rs
+++ topics/pattern_match/exercises/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn num_to_string(num: u32) -> String {
-    todo!();
+    match num{
+        0=>String::from("zero"),
+        1 => String::from("one"),
+        2=>String::from("two"),
+        3=>String::from("three"),
+        _=>String::from("other"),
+    }
 }
 
 pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
-    todo!();
+    let z= match x{
+        None=>v,
+        Some(val)=>val,
+    };
+    z
 }
