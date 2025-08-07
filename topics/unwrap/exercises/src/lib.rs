f36cb743caf35acc9adbeebcb3db0690d7822962
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2025-08-07 02:07:30 -0700

    finish unwrap lesson

--- topics/unwrap/exercises/src/lib.rs
+++ topics/unwrap/exercises/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn parse_and_add(a: &str, b: &str) -> u32 {
-    todo!();
+ let a:u32=a.parse().expect("Failed to parse variable");
+ let b:u32=b.parse().expect("Failed to parse variable");
+ b+a
 }
 
 pub fn unwrap_and_add(x: Option<u32>, y: Option<u32>) -> u32 {
-    todo!();
+  let z= x.unwrap()+ y.unwrap();
+  z
 }
